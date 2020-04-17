08fcee6a8d54ee4661e112a53aef4e37a4ee4e37
JakeSCahill <jake.cahill@iota.org>
2020-04-17 15:24:06 +0100

    Start Channels example

---
+++ src/bin/subscriber.rs
@@ -0,0 +1,36 @@
+use failure::{Fallible};
+use iota_lib_rs::prelude::iota_client;
+use iota_streams::app_channels::{
+    api::tangle::{Address, Message, Transport}
+};
+
+fn receive_messages<T>(client: &mut T, link: &Address) -> Fallible<Vec<Message>>
+where
+    T: Transport,
+    // Use the default options
+    <T>::RecvOptions: Copy + Default,
+{
+    client.recv_messages_with_options(link, T::RecvOptions::default())
+}
+
+fn main() -> Fallible<()> {
+    // tangle client
+    let mut client = iota_client::Client::new("https://nodes.devnet.iota.org:443");
+
+    // Subscribers need to channel address and the message identifier to be able to find messages on a channel
+    let channel_address =
+        "VZGHRWHIYKQBOMWSNRFGT9VAXPZASVOPGLYHBIV9NTTAAVAVHTMOZO9XHDDRDGADHRPJWWGJJEWLWPQXY";
+    let message_identifier = "UV9QBYJRVURWYGFIZENHOLUL9DD";
+
+    // Convert the channel address and message identifier to a Tangle Address type
+    let announcement_link = Address::from_str(channel_address, message_identifier).unwrap();
+
+    println!("Loading Announcement Message");
+    // Use the IOTA client to find transactions with the corresponding channel address and tag
+    let list = receive_messages(&mut client, &announcement_link)?;
+    for tx in list.iter() {
+        let header = tx.parse_header()?;
+        println!("Found {} message", header.content_type());
+    }
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use iota_lib_rs::prelude::iota_client;
+use iota_streams::{
+    app_channels::api::tangle::{Author, Transport}
+};
+use std::string::ToString;
+use failure::Fallible;
+
+fn start_a_new_channel<T>(client: &mut T) -> Fallible<()> where
+T: Transport,
+<T>::SendOptions: Copy + Default,
+{
+    // Create a new channel
+    let mut author = Author::new("AUTHORSSECRET", 3, false);
+    // Create an `Announce` message to start the channel
+    let announcement_result = author.announce()?;
+    // Print the information that needs to be sent to subscribers before they can read the message
+    println!("Address: {}", announcement_result.link.appinst.to_string());
+    println!("tag: {}", announcement_result.link.msgid.to_string());
+    // Convert the message to a bundle and send it to a node
+    client.send_message(&announcement_result)?;
+    Ok(())
+}
+
+fn main() {
+    // Connect to a node and pass this object to the function
+    let mut client = iota_client::Client::new("https://nodes.devnet.iota.org:443");
+    match start_a_new_channel(&mut client) {
+        Ok(()) => (),
+        Err(error) => println!("failed with error {}", error),
+    }
+}

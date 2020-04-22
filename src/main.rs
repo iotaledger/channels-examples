99956a347ca8c0ddfcdbc4d9e0bff21b5e2247e5
Jake Cahill <45230295+JakeSCahill@users.noreply.github.com>
2020-04-22 17:00:33 +0100

    Disable local proof of work
    Local proof of work is currently broken, this PR disables it in favor of remote proof of work.

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,8 @@ fn main() {
     // Change the default settings to use a lower minimum weight magnitude for the Devnet
     let mut send_opt = SendTrytesOptions::default();
     // default is 14
-    send_opt.min_weight_magnitude = 9; 
+    send_opt.min_weight_magnitude = 9;
+    send_opt.local_pow = false;
     match start_a_new_channel(&mut client, send_opt) {
         Ok(()) => (),
         Err(error) => println!("failed with error {}", error),
